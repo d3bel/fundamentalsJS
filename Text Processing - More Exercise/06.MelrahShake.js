3ccbaedcfe79f61e26f5f867041052dd0a82e389
Ivaylo Stefanov <105991192+d3bel@users.noreply.github.com>
2022-07-18 14:10:11 +0300

    Add files via upload

---
+++ Text Processing - More Exercise/06.MelrahShake.js
@@ -0,0 +1,28 @@
+function melrahShake(array) {
+  let str = array[0];
+  let pattern = array[1];
+
+  while (pattern.length > 0) {
+    if (
+      str.indexOf(pattern) !== str.lastIndexOf(pattern) &&
+      str.indexOf(pattern) > -1 &&
+      str.lastIndexOf(pattern) > -1
+    ) {
+      let firstPattern = str.indexOf(pattern);
+      let lastPattern = str.lastIndexOf(pattern);
+      str = str.split("");
+      let strRem = str.splice(lastPattern, pattern.length);
+      strRem = str.splice(firstPattern, pattern.length);
+      str = str.join("");
+      let removedPattern = pattern.split("").splice(pattern.length / 2, 1);
+      pattern = pattern.replace(removedPattern, "");
+      console.log("Shaked it.");
+    } else {
+      break;
+    }
+  }
+  console.log("No shake.");
+  console.log(str);
+}
+melrahShake(["astalavista baby", "sta"]);
+melrahShake(["##mtm!!mm.mm*mtm.#", "mtm"]);
